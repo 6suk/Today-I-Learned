f5cc28f401ecb7b56724c09510f53a2d51d79893
6suk <koandjo9116@gmail.com>
2022-11-21 23:51:57 +0900

    docs : gitignore

--- Baekjoon/src/backjoon_202211/Main_1316.java
+++ Baekjoon/src/backjoon_202211/Main_1316.java
@@ -1,24 +1,56 @@
 package backjoon_202211;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main_1316 {
 
 	public static void main(String[] args) {
+		int[] tmp = new int[26];
+
+		String s1 = "abcabc";
+		String s2 = "new";
+		String s3 = "year";
+		String[] str = s1.split("");
+		
 		List<String> list = new ArrayList<>();
-		list.add("happy");
-		list.add("new");
-		list.add("year");
 		
+		for (int i = 0; i < s1.length(); i++) {
+			tmp[s1.charAt(i)-97] += 1;
+			if(tmp[s1.charAt(i)-97] > 1) {
+				list.add(String.valueOf(s1.charAt(i)));
+			}
+		}
 		
-		list.stream()
-		.flatMap(data -> Arrays.stream(data.split(" "))
-		.forEach(x -> System.out.println(x));
+		list.forEach(x -> System.out.println(x));
 		
-		System.out.println(count);
+		int cnt = 0;
+		int result = 0;
+		int result2 = 0;
+		int startIndex = 0;
 		
+		for(int a : tmp) {
+			if(a > 1) {
+				String s = String.valueOf((char)(cnt+97));
+				startIndex = Arrays.binarySearch(str, s);
+				result = cnt;
+				result2 = a;
+			}
+			cnt++;
+		}
+		
+		int sum = 0;
+		for(int i = 0; i < 3; i++) {
+			sum += (startIndex+i);
+		}
+		
+		System.out.println(startIndex);
+		System.out.println(result);
+		System.out.println(result2);
+		System.out.println(Arrays.toString(tmp));
+		
+
 	}
+	
+
 
 }
