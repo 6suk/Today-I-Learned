dc79940dfa22cfc47b76b4f51c24f5195adcef80
6suk <koandjo9116@gmail.com>
2023-05-31 15:48:26 +0900

    Coding Test : 2023-05-31

---
+++ Baekjoon/src/backjoon_202305/Pro_176963.java
@@ -0,0 +1,35 @@
+package backjoon_202305;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Pro_176963 {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < name.length; i++) {
+            map.put(name[i], yearning[i]);
+        }
+
+        int[] result = new int[photo.length];
+
+        for(int i = 0; i < photo.length; i++){
+            int cnt = 0;
+            for(String p_name : photo[i]){
+                if(map.containsKey(p_name)){
+                    cnt += map.get(p_name);
+                }
+            }
+            result[i] = cnt;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] name = {"may", "kein", "kain", "radi"};
+        int[] yearning = {5, 10, 1, 3};
+        String[][] photo = {{"may", "kein", "kain", "radi"}, {"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+
+        System.out.println(Arrays.toString(new Pro_176963().solution(name, yearning, photo)));
+    }
+}
